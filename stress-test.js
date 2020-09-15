98dd4db4e9c10c618eeffac2baead330caab856b
Rafa JS <rjseibane@gmail.com>
2020-09-15 17:17:40 +0200

    Improve final report

--- stress-test.js
+++ stress-test.js
@@ -3,10 +3,12 @@
 const {HandCashCloudAccount, Environments} = require('@handcash/handcash-connect-beta');
 const pLimit = require('p-limit');
 
-const limit = pLimit(5);
+const limit = pLimit(3);
 
 (async () => {
     try {
+        var successCount = 0;
+        var errorCount = 0;
         const cloudAccount = HandCashCloudAccount.fromAuthToken(
             'd42ce7ab6f79431e32af1204c44f5b91d69fe6a06846e124a15b580e2f280545',
             Environments.iae,
@@ -18,16 +20,19 @@ const limit = pLimit(5);
             Array(50)
                 .fill(0)
                 .map(() => limit(() => cloudAccount.wallet.pay({
-                    description: 'Stress test',
+                    description: 'Pew pew',
+                    appAction: 'tip',
                     payments: [
                         {destination: publicProfile.handle, currencyCode: 'USD', sendAmount: 0.005},
                     ],
-                }))
+                    attachment: {format: 'base64', value: 'cGV3IHBldyBwZXc='}
+                })
                     .then(paymentResult => console.log(JSON.stringify(paymentResult)))
-                    .catch(error => console.error(JSON.stringify(error))))
-        );
+                    .then(_ => successCount++)
+                    .catch(error => console.error(JSON.stringify(error)))
+                    .then(_ => errorCount++))));
     } catch (e) {
         console.error(e);
     }
-    console.error('Stress Test Completed');
+    console.error(`Stress Test Completed - Successes: ${successCount}, Errors: ${errorCount}`);
 })();
